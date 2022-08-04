9b11d603149cc0443884d180ac8b7fbf2f625af2
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-04 15:26:52 -0600

    feat: add discount delete feature with tests"

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -17,10 +17,15 @@ class MerchantDiscountsController < ApplicationController
         discount = merchant.discounts.new(percent: params[:percent], quantity_threshold: params[:quantity_threshold])
         if discount.save
           redirect_to "/merchants/#{merchant.id}/discounts"
-      else
+        else
           redirect_to "/merchants/#{merchant.id}/discounts/new"
           flash[:alert] = "Error: Please fill out all required fields!"
-      end
+        end
+    end 
+
+    def destroy
+        Discount.find(params[:id]).destroy 
+        redirect_to "/merchants/#{params[:merchant_id]}/discounts"
     end 
 
     # private 
--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -41,4 +41,23 @@ RSpec.describe "merchant discounts index", type: :feature do
         click_on("Create a New Discount")
         expect(current_path).to eq("/merchants/#{merchant_1.id}/discounts/new")
     end
+
+    it 'can delete discounts' do
+        merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+    
+        discount1 = Discount.create!(percent: 20, quantity_threshold: 5, merchant_id: merchant_1.id)
+        discount2 = Discount.create!(percent: 10, quantity_threshold: 3, merchant_id: merchant_1.id)
+        discount3 = Discount.create!(percent: 25, quantity_threshold: 10, merchant_id: merchant_1.id)
+
+        visit "/merchants/#{merchant_1.id}/discounts"
+
+        click_button("Delete Discount: #{discount1.id}")
+
+        expect(current_path).to eq("/merchants/#{merchant_1.id}/discounts")
+        expect(page).to_not have_content("Percent Discount: 20%")
+        expect(page).to_not have_content("Quantity Threshold: 5")
+
+        expect(page).to have_content("Percent Discount: 10%")
+        expect(page).to have_content("Quantity Threshold: 3")
+    end
 end 
