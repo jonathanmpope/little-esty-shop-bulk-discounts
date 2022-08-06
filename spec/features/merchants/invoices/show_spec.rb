1d161921c81d4f355bdba6bef8fb0a007467cdb9
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-06 11:42:36 -0600

    test: add merchant invoice show page example 2 feature test

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -113,6 +113,27 @@ RSpec.describe 'invoices show page' do
         expect(page).to have_content("Total Merchant Revenue After Discounts: $350.00")
      end 
 
+      it 'has shows the total for an invoice - both total and discounted - example 2' do
+        merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+
+        item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 3000, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+        item_2 = Item.create!(name: "Crocs", description: "Worst and Best Shoes", unit_price: 4000, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+        
+        customer_1 = Customer.create!(first_name: "James", last_name: "Franco", created_at: Time.now, updated_at: Time.now)
+        
+        invoice_1 = customer_1.invoices.create!(status: 1, created_at: Time.now, updated_at: Time.now)
+        
+        invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 10, unit_price: item_1.unit_price, status: 0, created_at: Time.now, updated_at: Time.now)
+        invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_2.unit_price, status: 0, created_at: Time.now, updated_at: Time.now)
+
+        discount1 = Discount.create!(percent: 20, quantity_threshold: 10, merchant_id: merchant_1.id)
+
+        visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
+
+        expect(page).to have_content("Total Merchant Revenue Before Discounts: $500.00")
+        expect(page).to have_content("Total Merchant Revenue After Discounts: $440.00")
+     end 
+
      it 'has shows the total for an invoice - both total and discounted' do
         merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
         merchant_2 = Merchant.create!(name: "Barney", created_at: Time.now, updated_at: Time.now)
